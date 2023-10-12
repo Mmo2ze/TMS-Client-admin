ee2dfeca2f3031910c05a2f277cf88307fd66031
kindreklamar <139592450+kindreklamar@users.noreply.github.com>
2023-10-12 17:55:35 +0300

    login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,26 +1,102 @@
-import React from 'react'
+'use client'
+import React , {useState} from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const isEgyptianNumber = (number) => {
+  const pattern = /^01\d{9}$/;
+  return pattern.test(number);
+};
 
 const page = () => {
-  return (
+  const [sentcode, setSentcode] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState('');
 
-    <section className="bg-gray-50 dark:bg-gray-900">
-    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+  const handleSentCode = (e) => {
+    e.preventDefault();
+    if (isEgyptianNumber(phoneNumber)) {
+      setSentcode(true);
+    } else {
+      toast.error('الرقم المدخل غير صحيح. يجب أن يكون بصيغة مصرية صحيحة.');
+    }
+  };
+
+  return (
+    <> 
+   {sentcode ? ( <section className="  bg-gray-900">
+    <div className="flex direction-r flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+
+      <div className="w-full  rounded-lg shadow  border md:mt-0 sm:max-w-md xl:p-0  bg-gray-800  border-gray-700">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 className="text-xl direation-r font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-          تسجيل الدخول
+          <h1 className="text-xl font-bold leading-tight tracking-tightmd:text-2xl  text-white">
+        ادخل الكود
           </h1>
-          <form className="space-y-4 direation-r md:space-y-6" action="#">
+          <form className="space-y-4 md:space-y-6" action="#">
             <div>
-              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ادخل هاتفك</label>
-              <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+              <input type="number" name="number" id="number" className=" border direction-l  sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  bg-gray-700  border-gray-600  placeholder-gray-400  text-white  " placeholder="695318" required />
             </div>
-            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">ارسل رقم التحقق</button>
+    
+
+            <button type="submit" className="w-full text-white bg-blue-600  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  0  ">ارسل  الكود</button>
+
+
+
           </form>
         </div>
       </div>
     </div>
   </section>
+    ) : (
+        <section className="bg-gray-900">
+        <div className="flex direction-r flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
+                تسجيل الدخول
+              </h1>
+              <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label htmlFor="number" className="block mb-2 text-sm font-medium text-white">
+                    ادخل هاتفك
+                  </label>
+                  <input
+                    type="number"
+                    name="number"
+                    id="number"
+                    className="border sm:text-sm rounded-lg direction-l  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="0123456789"
+                    required
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                  />
+                </div>
+                <button
+                  type="submit"
+                  onClick={handleSentCode}
+                  className="w-full text-white  hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover-bg-primary-700 focus-ring-primary-800"
+                >
+                  ارسل رقم التحقيق
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    )}
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+    />
+
+  </>
   )
 }
 
